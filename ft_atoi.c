0cc5970fbe913902668fca1f87b2850b34180fc5
Aymeric Muhlethaler <amuhleth@c2r5s2.42lausanne.ch>
2021-10-25 18:07:34 +0200

    Atoi done i think

---
+++ ft_atoi.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amuhleth <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/25 17:47:41 by amuhleth          #+#    #+#             */
+/*   Updated: 2021/10/25 18:07:04 by amuhleth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_atoi(const char *str);
+
+int	ft_atoi(const char *str)
+{
+	unsigned int	nb;
+	int				sign;
+
+	nb = 0;
+	sign = 1;
+	if (*str == '-')
+	{
+		str++;
+		sign = -1;
+	}
+	if (*str == '+' && sign == 1)
+		str++;
+	while ('0' <= *str && *str <= '9')
+	{
+		nb = (*str - '0') + (nb * 10);
+		str++;
+	}
+	return (nb * sign);
+}
+
+int	main(int argc, char **argv)
+{
+	(void) argc;
+	printf("Lib : %d\n", atoi(argv[1]));
+	printf("Cust : %d\n", ft_atoi(argv[1]));
+	return (0);
+}
