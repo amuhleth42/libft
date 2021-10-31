df5d7e84ed5f283e03a21a73806c87b076054bd5
Aymeric Muhlethaler <amuhleth@c1r11s6.42lausanne.ch>
2021-10-31 16:22:28 +0100

    libft.h up to date

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: amuhleth <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 13:51:02 by amuhleth          #+#    #+#             */
-/*   Updated: 2021/10/27 19:26:58 by amuhleth         ###   ########.fr       */
+/*   Updated: 2021/10/31 16:22:08 by amuhleth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,38 @@
 
 # include <stdlib.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
 int	ft_atoi(const char *str);
-int	ft_isspace(int c);
+
+void	ft_bzero(void *s, size_t n);
+
+int	ft_isalnum(int c);
+
+int	ft_isalpha(int c);
+
+int	ft_isascii(int c);
+
+int	ft_isdigit(int c);
+
+int	ft_isprint(int c);
+
+void	*ft_memchr(const void *s, int c, size_t n);
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
+
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+
+void	*ft_memset(void *b, int c, size_t len);
+
+char	*ft_strchr(const char *s, int c);
+
+size_t	ft_strlcat(char *dst, char *src, size_t dstsize);
+
 size_t	ft_strlen(const char *s);
+
+char	*ft_strrchr(const char *s, int c);
+
+int	ft_tolower(int c);
+
+int	ft_toupper(int c);
 
 #endif
